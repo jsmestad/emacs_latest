81de8cf75f1503f68b25c42340b027b9f779ca07
jsmestad <justin.smestad@gmail.com>
2009-09-29 14:21:33 -0600

    initial

---
+++ install.rb
@@ -0,0 +1,29 @@
+# this is not the ruby you're looking for
+require 'fileutils'
+ 
+alias :V :lambda
+ 
+Joiner = V do |base|
+  V do |*others|
+    File.join(base, *others)
+  end
+end
+ 
+Home = Joiner[ File.expand_path( '~' ) ]
+Cwd  = Joiner[ File.expand_path(File.dirname(__FILE__)) ]
+ 
+Link = V do |target, new|
+  FileUtils.ln_s Cwd[ target ], Home[ new ] rescue puts("~/#{new} exists.")
+end
+ 
+Link[ 'emacs.el', '.emacs' ]
+Link[ '.', '.emacs.d' ]
+ 
+Git = V do |command, dir|
+  Dir.chdir(dir) do
+    `git #{command}`
+  end
+end
+ 
+`git submodule update --init`
+`cd vendor/rinari && git submodule update --init`
---
+++ jsmestad.el
@@ -0,0 +1,31 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+ 
+; custom place to save customizations
+(setq custom-file "~/.emacs.d/defunkt/custom.el")
+(load custom-file)
+ 
+(when (file-exists-p ".passwords") (load ".passwords"))
+ 
+(load "defunkt/lisp")
+(load "defunkt/global")
+(load "defunkt/defuns")
+(load "defunkt/bindings")
+(load "defunkt/modes")
+(load "defunkt/theme")
+(load "defunkt/temp_files")
+(load "defunkt/github")
+(load "defunkt/git")
+ 
+(when (file-exists-p "defunkt/private") 
+  (load "defunkt/private"))
+ 
+(vendor 'ack)
+(vendor 'cheat)
+(vendor 'magit)
+(vendor 'gist)
+(vendor 'growl)
+(vendor 'twittering-mode)
+(vendor 'textile-mode)
+(vendor 'textmate)
+(textmate-mode)
+(vendor 'yaml-mode)
